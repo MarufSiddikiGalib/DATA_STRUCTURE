1b4cc4f72b36b4f75b7bd8a0d7d62d2dcbd9ef84
Maruf Siddiki Galib <145563201+MarufSiddikiGalib@users.noreply.github.com>
2024-02-17 00:03:29 +0600

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int *pointvar, var;
+
+    var  = 5;
+
+    pointvar = &var;
+
+    cout<<pointvar<<endl;
+    cout<<var<<endl;
+    cout<<&pointvar<<endl;
+    cout<<&var<<endl;
+    cout<<*pointvar<<endl;
+
+
+
+
+
+
+
+}
+
